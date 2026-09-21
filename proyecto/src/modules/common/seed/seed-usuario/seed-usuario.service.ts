6dcb69c0ad0aa08708b4ed40a2750fe9bde53615
JulianTabo <tabox4ever@gmail.com>
2026-09-21 01:27:07 -0300

    fix: eliminar coma doble en entryData del seed de usuario

--- proyecto/src/modules/common/seed/seed-usuario/seed-usuario.service.ts
+++ proyecto/src/modules/common/seed/seed-usuario/seed-usuario.service.ts
@@ -53,7 +53,7 @@ export class SeedUsuarioService {
  // Seed de Usuarios
 async seedUsuario() {
   const entryData = [
-    { mail: 'superadmin@gmail.com', contrasena: 'Admin1234', rol: "Administrador", denominacion:"Administrador" },,
+    { mail: 'superadmin@gmail.com', contrasena: 'Admin1234', rol: "Administrador", denominacion:"Administrador" },
   ];
 
   for (const data of entryData) {
